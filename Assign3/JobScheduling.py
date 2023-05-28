27fad7010ca9edc6561983d8407bc3871822684b
spos1 <118359808+spos1@users.noreply.github.com>
2023-05-28 23:12:04 +0530

    Update JobScheduling.py

--- Assign3/JobScheduling.py
+++ Assign3/JobScheduling.py
@@ -1,17 +1,28 @@
-n = int(input("Enter number of jobs: "))
+def schedule_jobs(jobs):
+    sorted_jobs = sorted(jobs, key=lambda x: x[1], reverse=True)
+    schedule = []
+    current_time = 0
+
+    for job in sorted_jobs:
+        job_id, duration = job
+        start_time = current_time
+        finish_time = current_time + duration
+        current_time = finish_time
+        schedule.append((job_id, start_time, finish_time))
+
+    return schedule
+
+
+num_jobs = int(input("Enter the number of jobs: "))
 jobs = []
-print("Enter Id and profit respectively for each job:")
-for i in range(n):
-    job = input("Job " + str(i + 1) + ": ").split()
-    jobs.append(job)
+for i in range(num_jobs):
+    job_id = input(f"Enter the job ID for job {i+1}: ")
+    duration = int(input(f"Enter the duration for job {i+1}: "))
+    jobs.append((job_id, duration))
 
-sorter = lambda job: int(job[1])
-jobs = sorted(jobs, key=sorter, reverse=True)
 
-scheduled = []
+schedule = schedule_jobs(jobs)
 
-for i in jobs:
-    scheduled.append(i[0])
 
-print("Jobs are scheduled as: ")
-print(scheduled)
+for job in schedule:
+    print(f"Job {job[0]}: start time = {job[1]}, finish time = {job[2]}")
